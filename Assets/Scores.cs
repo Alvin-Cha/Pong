a3f65b1587b01fea022b8f5b39ab96ea16e19c44
Alvin.C <alvinschoolemail122@gmail.com>
2025-06-10 23:39:55 +0700

    1 Player Ai

--- Assets/Scores.cs
+++ Assets/Scores.cs
@@ -7,25 +7,21 @@ public class Scores : MonoBehaviour
     int p1_scores;
     int p2_scores;
 
-    // Public properties to get scores
     public int Player1Score => p1_scores;
     public int Player2Score => p2_scores;
 
-    // Method to add score to Player 1
     public void AddScoreToPlayer1(int score)
     {
         p1_scores += score;
         Debug.Log("Player 1 Score: " + p1_scores);
     }
 
-    // Method to add score to Player 2
     public void AddScoreToPlayer2(int score)
     {
         p2_scores += score;
         Debug.Log("Player 2 Score: " + p2_scores);
     }
 
-    // Method to reset scores
     public void ResetScores()
     {
         p1_scores = 0;
@@ -33,7 +29,6 @@ public class Scores : MonoBehaviour
         Debug.Log("Scores Reset!");
     }
 
-    // Optional: Display scores in Update for testing
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
---
+++ Assets/enemy_Ai.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_Ai : MonoBehaviour
+var speed : float = 80;
+var Baller : Transform;
+private var d : float;
+var move : Vector3;
+
+function Update () {
+	d = Ball.position.y - transform.position.y;
+	if (d > 0)
+	{
+		move.y = speed * Mathf.Min(d, 40);
+	}	
+	if (d < 0)
+	{
+		move.y = -speed * Mathf.Min(-d, 40);
+	}
+}
+}
