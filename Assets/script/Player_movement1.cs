8db4b41e01d4bc9b0ef5f39b118b3ac51de184cd
Alvin.C <alvinschoolemail122@gmail.com>
2025-06-17 23:35:58 +0700

    ground check

--- Assets/script/Player_movement1.cs
+++ Assets/script/Player_movement1.cs
@@ -1,22 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_movement1 : MonoBehaviour
 {
-    public float jump_force = 5f;
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public float checkRadius = 0.1f;
+    public LayerMask groundLayer;
+
+    [SerializeField] private SpriteRenderer spriterenderer;
+
+    private float playerHalfHeight;
+
     private Rigidbody2D rb;
+    private bool isGrounded;
 
     void Start()
     {
+        playerHalfHeight = spriterenderer.bounds.extents.y;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        bool hit = Physics2D.Raycast(transform.position, Vector2.down, playerHalfHeight + 0.1f, LayerMask.GetMask("ground"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && hit)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jump_force);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+
     }
 }
