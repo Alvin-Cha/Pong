858f113fa68d473950847702dd8cc9dd9e9fc011
Alvin.C <alvinschoolemail122@gmail.com>
2025-06-17 22:44:56 +0700

    chest and ship update

--- Assets/script/Player_movement2.cs
+++ Assets/script/Player_movement2.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class Player_movement2 : MonoBehaviour
 {
-    public float move_speed;
+    public float jump_force = 5f;
+    private Rigidbody2D rb;
 
-    void Update() {
-        bool is_pressing_w = Input.GetKey(KeyCode.UpArrow);
-        bool is_pressing_s = Input.GetKey(KeyCode.DownArrow);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-        if(transform.position.y >= 1.25f){
-            transform.position = new Vector2(transform.position.x, 1.24f);
-        } else if (is_pressing_w){
-            transform.Translate(Vector2.right * Time.deltaTime * move_speed);
-        }
-        
-        if(transform.position.y <= -1.25f){
-            transform.position = new Vector2(transform.position.x, -1.24f);
-        }else if(is_pressing_s){
-            transform.Translate(Vector2.left * Time.deltaTime * move_speed);
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) // Enter key
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_force);
         }
     }
 }
---
+++ Assets/script/playerjump.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerjump : MonoBehaviour
+{
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public float checkRadius = 0.1f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
